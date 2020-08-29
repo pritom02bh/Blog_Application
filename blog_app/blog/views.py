a5abdc85a554e058f3b44b29f7b3b0850d63d5b6
Pritom Bhowmik <56447975+pritom02bh@users.noreply.github.com>
2020-08-29 23:12:09 +0600

    blog application

--- blog_app/blog/views.py
+++ blog_app/blog/views.py
@@ -1,5 +1,23 @@
 from django.shortcuts import render
 
+post = [
+    {
+        'author': 'Pritom Bhowmik',
+        'title': 'Deep Learning',
+        'content': 'Artificial Neural Network',
+        'date_posted': 'August 29, 2020'
+    },
+
+    {
+        'author': 'Andrew Ng',
+        'title': 'Todays Machine Learning',
+        'content': 'Machine Learning is a new Electricity',
+        'date_posted': 'August 25, 2020'
+
+    }
+
+]
+
 
 def home(request):
     return render(request, 'blog/home.html')
